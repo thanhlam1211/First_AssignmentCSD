50bc502bf8a7f10dc8037547512d7530354e1451
Thanh Lâm <57887863+thanhlam1211@users.noreply.github.com>
2021-02-02 17:15:39 +0700

    Update ProductInter.java

--- Assignment_CSD_1/src/Intermediaries/ProductInter.java
+++ Assignment_CSD_1/src/Intermediaries/ProductInter.java
@@ -30,6 +30,7 @@ public class ProductInter {
     public static void addProduct(String pcode, String pro_name, int quantity, int saled, double price) {
         Product product = new Product(pcode, pro_name, quantity, saled, price);
         Manager.addProductLast(product);
+
     }
 
     public static void addProductIndex(String pcode, String pro_name, int quantity, int saled, double price, int index) {
@@ -43,13 +44,13 @@ public class ProductInter {
             System.err.println("Empty list!");
             return;
         }
-        System.out.printf("%5s|%11s|%10s|%8s|%8s|%8s\n", "pcode", "pro_name", "quantity", "saled", "price","value");
+        System.out.printf("%5s|%11s|%10s|%8s|%8s|%8s\n", "pcode", "pro_name", "quantity", "saled", "price", "value");
         System.out.println("=======================================================");
         for (int i = 0; i < prodductList.size(); i++) {
             Product currentProduct = prodductList.get(i);
             System.out.printf("%5s|%11s|%10s|%8s|%8s|%8s\n", currentProduct.getPcode(), currentProduct.getPro_name(),
                     currentProduct.getQuantity(), currentProduct.getSaled(), currentProduct.getPrice(),
-                    currentProduct.getQuantity() * currentProduct.getSaled());
+                    currentProduct.getPrice() * currentProduct.getSaled());
         }
     }
 
@@ -79,19 +80,20 @@ public class ProductInter {
     }
 
     public void deleteBefore(String pcode) {
-        if(Manager.checkExistProduct(pcode)) {
+        if (Manager.checkExistProduct(pcode)) {
             int index = Manager.findProductIndex(pcode);
-            if(index == 0) {
+            if (index == 0) {
                 System.err.println("Can not delete because node having pcode: " + pcode + " is the first node!");
                 return;
             }
-            Manager.deleteProductByIndex(index - 1);
             System.err.println("Delete Success!");
+            Manager.deleteProductByIndex(index - 1);
             return;
         }
         System.err.println("Product doesn't exist!");
     }
-    public boolean checkInputIndex(int index){
+
+    public boolean checkInputIndex(int index) {
         return Manager.checkInputIndex(index);
     }
 }
