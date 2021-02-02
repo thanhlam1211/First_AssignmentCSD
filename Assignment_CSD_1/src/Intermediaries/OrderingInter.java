e95ad2f63c81a753bcb383c2632e458989e99124
Thanh Lâm <57887863+thanhlam1211@users.noreply.github.com>
2021-02-02 17:16:25 +0700

    Update OrderingInter.java

--- Assignment_CSD_1/src/Intermediaries/OrderingInter.java
+++ Assignment_CSD_1/src/Intermediaries/OrderingInter.java
@@ -22,7 +22,7 @@ public class OrderingInter {
     public void add(String pcode, String ccode, int quantity) {
         Product product = Manager.getProduct(pcode);
         Ordering ordering;
-        if (Manager.checkExistProduct(ccode) && Manager.checkCustomerExist(ccode)) {
+        if (Manager.checkExistProduct(pcode) && Manager.checkCustomerExist(ccode)) {
             if (Manager.checkExistOrdering(pcode, ccode) == -1) {
                 if (product.getQuantity() == product.getSaled()) {
                     System.err.println("Product is exhausted!");
@@ -31,11 +31,14 @@ public class OrderingInter {
                     Manager.updateOrder(pcode);
                     Manager.addOrder(ordering);
                 }
-            } else if (product.getSaled() < product.getQuantity()
-                    && quantity <= (product.getQuantity() - product.getSaled())) {
-                ordering = new Ordering(pcode, ccode, quantity);
-                Manager.updateOrder(pcode);
-                Manager.addOrder(ordering);
+                if (product.getSaled() < product.getQuantity()
+                        && quantity <= (product.getQuantity() - product.getSaled())) {
+                    ordering = new Ordering(pcode, ccode, quantity);
+                    Manager.updateOrder(pcode);
+                    Manager.addOrder(ordering);
+                } else {
+                    System.err.println("Invalid quantity! Try again.");
+                }
             }
         } else {
             System.err.println("Product or Customer not found! Try again");
@@ -46,7 +49,7 @@ public class OrderingInter {
         MyList<Ordering> orderingList = Manager.getOrderingList();
         System.out.printf("%5s|%5s|%5s\n", "Pcode", "Ccode", "Value");
 
-        for(int i = 0; i < orderingList.size(); ++i) {
+        for (int i = 0; i < orderingList.size(); ++i) {
             Ordering currentOrder = orderingList.get(i);
             System.out.printf("%5s|%5s|%5d\n", currentOrder.getPcode(), currentOrder.getCcode(),
                     currentOrder.getQuantity());
