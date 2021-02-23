d4fd368548b77aee342640bd931c35847c93044c
Thanh Lâm <57887863+thanhlam1211@users.noreply.github.com>
2021-02-23 13:31:36 +0700

    Update OrderingGUI.java

--- Assignment_CSD_1/src/GUI/OrderingGUI.java
+++ Assignment_CSD_1/src/GUI/OrderingGUI.java
@@ -17,7 +17,6 @@ public class OrderingGUI {
 
     static OrderingInter orderingInter = new OrderingInter();
 
-
     public static void menu() {
         System.out.println("3.1. Input data");
         System.out.println("3.2. Display data with total value");
@@ -47,16 +46,28 @@ public class OrderingGUI {
     public static void inputData() {
         System.out.print("Enter pcode: ");
         String pcode = Validate.checkInputString();
+        String pcode_checked = orderingInter.checkInputPcode(pcode);
+        if (pcode_checked == null) {
+            System.out.println("Pcode not found in Customer list");
+            return;
+        }
         System.out.print("Enter ccode: ");
         String ccode = Validate.checkInputString();
+        String ccode_checked = orderingInter.checkInputCcode(ccode);
+        if (ccode_checked == null) {
+            System.out.println("Ccode not found in Customer list");
+            return;
+        }
         System.out.print("Enter quantity: ");
         int quantity = Validate.checkInputInt();
-        orderingInter.add(pcode, ccode, quantity);
+        orderingInter.add(pcode_checked, ccode_checked, quantity);
     }
+
     public static void displayData() {
         orderingInter.display();
     }
-     private static void sortByBothCode() {
-       orderingInter.sortByBothCode();
+
+    private static void sortByBothCode() {
+        orderingInter.sortByBothCode();
     }
 }
